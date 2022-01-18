d73f800b8ac33e6a67a25f4ee3916c3e21884ee3
Alex Castro <acastro24@fordham.edu>
2022-01-18 15:03:48 -0500

    Update apctg-schema.php

--- apctg-schema.php
+++ apctg-schema.php
@@ -1,12 +1,12 @@
 <?php
 /**
  * Plugin Name: APCTG Schema for Bootstrap
- * Plugin URI:
+ * Plugin URI: https://github.com/alexpcastro/APCTG-Schema.org-WordPress-Plugin/
  * Description: Schema shortcodes built on bootstrap that work with Wordpress custom post types
  * Version: 1.0.0
  * Author: Alex Castro
  * Author URI: https://apctg.com
- * Author Email:
+ * Author Email: acastro24@fordham.edu
  * License: GPLv2
  */
 
